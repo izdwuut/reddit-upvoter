6350d0b433f320a1a18dc4b41a6674b3abe5b34d
Bartosz " izdwuut <izdwuut@gmail.com>
2018-10-21 13:52:46 +0200

    Initial version.

---
+++ upvoter.py
@@ -0,0 +1,35 @@
+import praw
+import time
+from _file import File
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('settings.ini')
+
+api_key = config['reddit']['api_key']
+client_id = config['reddit']['client_id']
+username = config['reddit']['username']
+password = config['reddit']['password']
+user_agent = config['reddit']['user_agent']
+
+api = praw.Reddit(client_id=client_id,
+                  client_secret=client_secret,
+                  user_agent=client_secret,
+                  username=username,
+                  password=password)
+
+threads = File('threads.txt')
+submissions = []
+for s in api.redditor('izdwuut').submissions.new():
+    if s.subreddit == 'GiftofGames':
+        if '[Offer]'.lower() in s.title.lower():
+            if s.over_18 and not threads.contains(s.fullname):
+                print('Processing: ' + s.title)
+                submissions.append(s)
+                for c in s.comments:
+                    c.upvote()
+                    print(c.fullname + ' upvoted!')
+                    time.sleep(2)
+                threads.add_line(s.fullname)
+                time.sleep(2)
+threads.close()
