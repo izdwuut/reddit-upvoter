2759e70e128c7c149911e6f6b434600c52032e03
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-26 15:10:06 +0100

    Refactor.

--- upvoter.py
+++ upvoter.py
@@ -4,19 +4,6 @@ from praw import Reddit
 from tqdm import tqdm
 
 class Upvoter:
-    _api = None
-
-    @property
-    def api(self):
-        if not self._api:
-            self._api = Reddit(client_id=self.config['reddit']['client_id'],
-                               client_secret=self.config['reddit']['client_secret'],
-                               user_agent=self.config['reddit']['user_agent'],
-                               username=self.config['reddit']['username'],
-                               password=self.config['reddit']['password'])
-
-        return self._api
-
     def upvote(self):
         for submission in self.api.subreddit(self.config['reddit']['subreddit']).stream.submissions():
             print('Processing thread {}.'.format(submission.title))
@@ -28,6 +15,11 @@ class Upvoter:
     def __init__(self, settings='settings.ini'):
         self.config = ConfigParser(os.environ)
         self.config.read(settings)
+        self.api = Reddit(client_id=self.config['reddit']['client_id'],
+                          client_secret=self.config['reddit']['client_secret'],
+                          user_agent=self.config['reddit']['user_agent'],
+                          username=self.config['reddit']['username'],
+                          password=self.config['reddit']['password'])
 
 
 if __name__ == '__main__':
